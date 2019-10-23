d796bb4973ef386dd1e493ca4bc1ebde57ac72c2
Djibril SY <dsy@e2r12p2.42.fr>
2019-10-23 16:31:10 +0200

    fixes

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dsy <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 17:23:39 by dsy               #+#    #+#             */
-/*   Updated: 2019/10/20 16:17:18 by dsy              ###   ########.fr       */
+/*   Updated: 2019/10/23 16:30:47 by dsy              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,11 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
-	if (ft_strlen(s1) == 0)
-		return (-(unsigned char)s2[i]);
-	if (ft_strlen(s2) == 0)
-		return ((unsigned char)s1[i]);
-	while (s1[i] && i < n)
-	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	while (*(s1 + i) && *(s1 + i) == *(s2 + i) && i < n - 1)
 		i++;
-	}
-	return (0);
+	if (n)
+		return (*((unsigned char *)s1 + i) - *((unsigned char *)s2 + i));
 }
