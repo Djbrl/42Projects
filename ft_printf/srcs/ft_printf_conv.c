43ee2693d2a0a5edf277238aceb0caf6a02f5c96
Djibril SY <dsy@e3r1p1.42.fr>
2019-12-02 22:30:51 +0100

    added f and hex conversion

--- ft_printf/srcs/ft_printf_conv.c
+++ ft_printf/srcs/ft_printf_conv.c
@@ -6,13 +6,71 @@
 /*   By: dsy <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:47:54 by dsy               #+#    #+#             */
-/*   Updated: 2019/12/02 22:10:51 by dsy              ###   ########.fr       */
+/*   Updated: 2019/12/02 22:29:25 by dsy              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libftprintf.h"
 
-void	ft_putnbr_x(unsigned long n)
+static unsigned long	ft_power(unsigned long x, int y)
+{
+	if (y == 0 || x == 1)
+		return (1);
+	if (x == 0 || y < 0)
+		return (0);
+	return (x * ft_power(x, --y));
+}
+
+void				ft_putnbr_x(unsigned long value)
+{
+	int				len;
+	unsigned long	rest;
+	char			*res;
+	int				base;
+
+	base = 16;
+	len = 0;
+	if (value < 10)
+		len = 1;
+	else
+		while (ft_power(base, len) <= value)
+			len++;
+	if (!(res = malloc(sizeof(char) * (len + 1))))
+		return ;
+	res[len] = 0;
+	if (value < 10)
+		res[0] = '0' + value;
+	else
+		while (--len >= 0)
+		{
+			rest = value % base;
+			res[len] = ((rest < 10) ? '0' : 'A' - 10) + rest;
+			value /= base;
+		}
+	ft_putstr(res);
+	free(res);
+}
+/*
+void	ft_putnbr_x(unsigned long value)
+{
+	int				len;
+	unsigned long	rest;
+	int base = 16;
+
+	len = 0;
+
+	if (value < 10)
+		ft_putchar(value + '0');
+	else
+		while (value > 0)
+		{
+			rest = value % base;
+			ft_putchar(((rest < 10) ? '0' : 'A' - 10) + rest);
+			value /= base;
+		}
+}
+
+*
 {
 	unsigned long nb;
 	char *base = "0132456789abcdef";
@@ -31,7 +89,7 @@ void	ft_putnbr_x(unsigned long n)
 	ft_putnbr_x(nb / 16);
 	ft_putchar(base[nb % 16]);
 	return ;
-}
+}*/
 
 int s_conversion(va_list params, char arg_type)
 {
@@ -56,7 +114,7 @@ int p_conversion(va_list params)
 {
 	void *p;
 	write(1, "0x", 2);
-	p = va_arg(params, void*);`
+	p = va_arg(params, void*);
 	ft_putnbr_x((unsigned long)p);
 	return (1);
 }
