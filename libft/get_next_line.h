430590c96d4c1833f6bc6084246aa8d6cbb3f0da
djbrl <sydismyname123@gmail.com>
2020-02-14 19:37:31 +0100

    added gnl to libft

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: dsy <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 02:17:42 by dsy               #+#    #+#             */
-/*   Updated: 2019/11/23 15:28:27 by dsy              ###   ########.fr       */
+/*   Updated: 2020/02/14 19:36:59 by dsy              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 64
 # define MAX_FD 1024
 # include <stdlib.h>
 # include <unistd.h>
