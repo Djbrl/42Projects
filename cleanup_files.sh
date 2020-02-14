a725373866e64277c952fee63a407e84cdc1b00a
djbrl <sydismyname123@gmail.com>
2020-02-14 20:15:27 +0100

    cleanup routine

---
+++ cleanup_files.sh
@@ -0,0 +1,9 @@
+find . -type f -name  "*.o" -exec rm -f {} \;
+find . -type f -name  "*.swp" -exec rm -f {} \;
+find . -type f -name  "a.out" -exec rm -f {} \;
+find . -type f -name  "*.a" -exec rm -f {} \;
+find . -type f -name  "*.he" -exec rm -f {} \;
+
+git add *
+git commit -m "cleanup routine"
+git push
