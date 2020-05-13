7a7936ec8abb8323269b81d0b6d2f333f407de07
Djbrl <sydjbrl@gmail.com>
2020-05-13 20:29:26 +0200

    [Progress saved.]

--- minishell/main.c
+++ minishell/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "../libft/libft.h"
 #include <string.h>
 #define BUF 4096
@@ -12,13 +14,45 @@ void flush_buffer(char *buffer)
 	ft_memset(buffer, 0, BUF);
 }
 
+void sanitize_args(char **args)
+{
+	int i = 0;
+	int j = 0;
+
+	while (args[i])
+	{
+		while(args[i][j])
+		{
+			if(args[i][j] == '\n')
+				args[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+}
+
 void evaluate_commands(char **args)
 {
-	int i;
-	
+	pid_t pid; 
+	pid_t wpid;
+	int status;
+
 	i = 0;
-	while(args[i])
-		ft_putstr(args[i++]);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+			perror("minishell");
+		exit(EXIT_FAILURE);
+	}
+	else if(pid < 0)
+		perror("fork error");
+	else
+	{
+		do
+			wpid = waitpid(pid, &status, WUNTRACED);
+		while(!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
 
 void shell_loop(void)
@@ -27,7 +61,8 @@ void shell_loop(void)
 	char **args;
 	int status;
 	char buffer[BUF];
-	printf("Welcome in dsyshell.\n");
+
+	printf("Welcome in minishell.\n");
 	while (1)
 	{
 		write(1, "> ", 2);
@@ -35,13 +70,13 @@ void shell_loop(void)
 		if (!(ft_strcmp(buffer, "exit\n")))
 			exit(EXIT_SUCCESS);
 		line = strdup(buffer);
-		args = ft_split(line, ';');
+		args = ft_split(line, ' ');
+	
+		sanitize_args(args);
 		evaluate_commands(args);
 		flush_buffer(buffer);
 		free(line);
 	}
-	//execute the command
-	//free stuff and update status/success/failure
 }
 
 int main(int ac, char **av)
@@ -49,11 +84,8 @@ int main(int ac, char **av)
 	int done;
 	int i = 0;
 	done = 0;
-	
+
 	shell_loop();
-//	char **s = ft_split("this is a test", ' ');
-//	while(s[i])
-//		printf("%s\n", s[i++]);
 	return (0);
 }
 
