f0ab10ae54f2c9b5ea70bcbaa02b115e0e3310a7
djbrl <sydismyname123@gmail.com>
2020-07-19 16:46:24 +0200

    [Progress saved.]

--- randtraining/misc/lists.c
+++ randtraining/misc/lists.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct	node{
-	int		val;
+	char*	track;
 	struct	node *next;
 }				t_node;
 
+int play_song(char *s, t_node songs);
+{
+
+}
+
 int main()
 {
-	node_t *head = NULL;
+	t_node *head = NULL;
+	t_node *second = NULL;
+	t_node *third = NULL;
 	
-	if (!(head = malloc(sizeof(node_t)))
-			return 0;
+	if (!(head = malloc(sizeof(t_node))))
+		return (0);
+	if (!(second = malloc(sizeof(t_node))))
+		return (0);
+	if (!(third = malloc(sizeof(t_node))))
+		return (0);
+
+	head->track	= "Future - Im so Groovy";
+	head->next = second;
+
+	second->track = "Future - Feds Did A Sweep";
+	second->next = third;
+	
+	third->track = "Kanye West - Wash Us In The Blood";
+	third->next = head;
+	return (0);
 }
