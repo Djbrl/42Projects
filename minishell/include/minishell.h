251a83910c53a20d53d202bfc849a08f2d3d7645
djbrl <sydismyname123@gmail.com>
2020-09-22 05:16:54 +0200

    [Progress saved.]

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -1,13 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsy <marvin@42.fr>                         +#+  +:+       +#+        */
+/*   By: dsy <dsy@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 16:15:31 by dsy               #+#    #+#             */
-/*   Updated: 2020/02/14 16:15:34 by dsy              ###   ########.fr       */
+/*   Updated: 2020/09/22 03:31:46 by dsy              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "libft.h"
+#include <string.h>
+#include <limits.h>
+#define BUF 4096
--- minishell/src/main.c
+++ minishell/src/main.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include "libft.h"
-#include <string.h>
-#include <limits.h>
-#define BUF 4096
+#include "minishell.h"
 
 /*
 Votre shell doit :
@@ -40,91 +32,6 @@ bash.
 
 • ctrl-C, ctrl-D et ctrl-\ doivent afficher le même résultat que dans bash.
 */
-
-void flush_buffer(char *buffer)
-{
-	int i;
-
-	i = 0;
-	ft_memset(buffer, 0, BUF);
-}
-
-void sanitize_args(char **args)
-{
-	int i = 0;
-	int j = 0;
-
-	while (args[i])
-	{
-		while(args[i][j])
-		{
-			if(args[i][j] == '\n')
-				args[i][j] = 0;
-			j++;
-		}
-		i++;
-	}
-}
-
-void echo(char *s)
-{
-	int i;
-	i = 0;
-	while (s[i])
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
-
-void echo_n(char *s)
-{
-	int i;
-	i = 0;
-	
-	if (!s)
-		return ;
-	while (s[i])
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-}
-
-void evaluate_commands(char **args)
-{
-	pid_t pid; 
-	pid_t wpid;
-	int status;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		if (!ft_strcmp(args[0], "echo") && !args[2])
-			echo(args[1]);
-		else if (!ft_strcmp(args[0], "echo") && !ft_strcmp(args[1], "-n"))
-			echo_n(args[2]);
-		else 
-			if (execvp(args[0], args) == -1)
-				perror("minishell");
-		exit(EXIT_FAILURE);
-	}
-	else if(pid < 0)
-		perror("fork error");
-	else
-	{
-		do
-			wpid = waitpid(pid, &status, WUNTRACED);
-		while(!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-}
-
-void signalHandler(int sig_n)
-{
-	write(1, "\n> ", 3);
-	//flush_buffer(buffer);
-}
 
 void shell_loop(void)
 {
