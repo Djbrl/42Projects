8c94d91eba121f85c40b55cd6fd2452441586324
Djibril SY <dsy@e2r10p9.clusters.42paris.fr>
2022-05-17 16:49:32 +0200

    checkpoint

---
+++ pipex/include/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsy <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/27 18:17:33 by dsy               #+#    #+#             */
+/*   Updated: 2022/05/17 16:23:27 by dsy              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <sys/types.h>
+# include "libft.h"
+
+# define INT_MIN -2147483648
+# define INT_MAX 2147483647
+
+//STACK FUNCTIONS
+
+//OPERATIONS
+
+//ALGORITHMS
+
+#endif
