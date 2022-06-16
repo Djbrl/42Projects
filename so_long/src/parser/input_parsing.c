492a9724bc1891823a2062bb6ddbb85787f28397
Djibril SY <dsy@e2r2p23.clusters.42paris.fr>
2022-06-16 15:25:05 +0200

    progress on parsing in check_map function

--- so_long/src/parser/input_parsing.c
+++ so_long/src/parser/input_parsing.c
@@ -25,37 +25,56 @@ int	check_map_extension(char *path)
 	return (0);
 }
 
-int	load_map_data(int file, t_game *data)
+char	*read_map_data(int file, char *path, t_game *data)
 {
 	int		file_size;
 	char	*map;
 	char	tmp[1];
-	int ret;
 
 	file_size = 0;
 	while (read(file, tmp, 1) > 0)
 		file_size++;
+	close(file);
+	file = open(path, O_RDONLY);
 	map = (char *)malloc(sizeof(char) * (file_size + 1));
-	if ((ret = read(file, map, file_size)) == -1)
+	if (read(file, map, file_size) == -1)
 	{
-		printf("couldnt load map");
+		free(map);
+		return (NULL);
+	}
+	close(file);
+	(void)data;
+	return (map);
+}
+
+int	check_map(char *map, t_game *data)
+{
+	char	**map_to_parse;
+	int		i;
+
+	i = 0;
+	if (ft_strlen(map) < 18)
+	{
 		free(map);
 		return (0);
 	}
-	printf("map loaded(ret %i) : \n%s\n", ret, map);
-	close(file);
+	(void)map_to_parse;
 	(void)data;
+	//map_to_parse = ft_split(map, '\n');
+	free(map);
 	return (1);
 }
 
-int	parse_map(char *map, t_game *data)
+int	parse_map(char *path, t_game *data)
 {
-	int file;
+	int		file;
+	char	*map;
 
-	file = open(map, O_RDONLY);
+	file = open(path, O_RDONLY);
 	if (file < 0)
 		return (0);
-	if (!load_map_data(file, data))
+	map = read_map_data(file, path, data);
+	if (!map || !check_map(map, data))
 		return (exit_error(MAP_INVALID, data));
 	return (file);
 }
