eaeed61995c81bbceb169150c793a060b5db1d83
djbrl <sydismyname123@gmail.com>
2022-06-17 04:49:07 +0000

    finished map parsing, unit test and norm to do

--- so_long/src/parser/input_parsing.c
+++ so_long/src/parser/input_parsing.c
@@ -46,22 +46,111 @@ char	*read_map_data(int file, char *path)
 	return (map);
 }
 
+int	get_map_heigth(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+		i++;
+	return (i);
+}
+
+int	get_map_width(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[0][i])
+		i++;
+	return (i);
+}
+
+int	check_map_content(char **map)
+{
+	int	i;
+	int	j;
+	int	height;
+	int width;
+
+	height = get_map_heigth(map) - 1;
+	width = get_map_width(map) - 1;
+	i = 0;
+	j = 0;
+	while (map[0][j])
+	{
+		if (map[0][j] != '1')
+			return (0);
+		j++;
+	}
+	j = 0;
+	while (map[height][j])
+	{
+		if (map[height][j] != '1')
+			return (0);
+		j++;
+	}
+	j = 0;
+	while (map[i])
+	{
+		if (map[i][0] != '1' || map[i][width] != '1')
+			return (0);
+		if (map[i][j] != 'C' && map[i][j] != 'P' && map[i][j] != 'E' \
+			&& map[i][j] != '0' && map[i][j] != '1')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int check_map_config(char **map)
+{
+	int	i;
+	int	j;
+	int	p_count;
+	int	c_count;
+	int	e_count;
+
+	i = 0;
+	j = 0;
+	p_count = 0;
+	c_count = 0;
+	e_count = 0;
+	if (!check_map_content(map))
+		return (0);
+	while (map[i])
+	{
+		while (map[i][j])
+		{
+			if (map[i][j] == 'C')
+				c_count++;
+			if (map[i][j] == 'P')
+				p_count++;
+			if (map[i][j] == 'E')
+				e_count++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	if (!c_count || !p_count || !e_count || p_count > 1)
+		return (0);
+	return (1);
+}
+
 int	check_map(char *map, t_game *data)
 {
 	char	**map_to_parse;
-	int		i;
 
-	i = 0;
 	if (ft_strlen(map) < 18)
 	{
 		free(map);
 		return (0);
 	}
-	(void)map_to_parse;
 	(void)data;
 	map_to_parse = ft_split(map, '\n');
-	while (map_to_parse[i])
-		printf("%s\n", map_to_parse[i++]);
+	if (!check_map_config(map_to_parse))
+		return (0);
 	free_split(map_to_parse);
 	free(map);
 	return (1);
