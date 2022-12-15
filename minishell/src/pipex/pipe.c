e8616ae4708480ec92de9f357a9a133487c0fb28
Djibril SY <dsy@e3r3p14.clusters.42paris.fr>
2022-12-15 15:22:11 +0100

    workingo n integration

--- minishell/src/pipex/pipe.c
+++ minishell/src/pipex/pipe.c
@@ -12,63 +12,107 @@
 
 #include "minishell.h"
 
-static int	pipe_cmd(t_msh *msh, char **expr, int j)
+static void connect_fds(t_expr **curr_command, t_expr *prev_command, t_expr *commands)
 {
-	char	*cmd;
-	char	*path;
-	int		status;
+	t_expr *cur;
+	int			pipefd[2];
 
-	status = 0;
-	if (access(expr[0], X_OK & F_OK) == 0)
-		execve(expr[0], expr, msh->envp);
-	cmd = ft_strjoin(msh->paths[j], "/");
-	path = ft_strjoin(cmd, expr[0]);
-	status = execve(path, expr, msh->envp);
-	free(cmd);
-	free(path);
-	return (status);
+	cur = *curr_command;
+	while (cur->next != NULL)
+	{
+		pipe(pipefd);
+		cur->fd_out = pipefd[1];
+		cur->next->fd_in = pipefd[0];
+		prev_command = cur;
+		cur = cur->next;
+	}
+	cur->fd_out = 1;
+	cur = commands;
 }
 
-static void	pipe_fork(t_msh *msh, char **expr, int fd[2])
-{
-	pid_t	pid;
-	int		j;
 
-	j = 0;
-	pid = fork();
-	if (pid == 0)
-	{
-		close(fd[0]);
-		dup2(fd[1], 1);
-		while (msh->paths[j])
-			pipe_cmd(msh, expr, j++);
+static void execute_commands(t_expr **curr_command)
+{
+	t_expr *cur;
+
+	cur = *curr_command;
+	if (cur->fd_in != 0) {
+		dup2(cur->fd_in, 0);
+		close(cur->fd_in);
 	}
-	else
-	{
-		close(fd[1]);
-		dup2(fd[0], 0);
-		waitpid(pid, &g_status, WUNTRACED);
-		while (!WIFEXITED(g_status) && !WIFSIGNALED(g_status))
-			waitpid(pid, &g_status, WUNTRACED);
+	if (cur->fd_out != 1) {
+		dup2(cur->fd_out, 1);
+		close(cur->fd_out);
 	}
+	execlp(cur->data, cur->data, NULL);
+	exit(EXIT_FAILURE);
+}
+
+static void close_fds(t_expr **curr_command)
+{
+	t_expr *cur;
+
+	cur = *curr_command;
+	if (cur->fd_in != 0) {
+		close(cur->fd_in);
+	}
+	if (cur->fd_out != 1) {
+		close(cur->fd_out);
+	}
+}
+
+static void	execute_multi_pipe(t_expr *commands) {
+	t_expr	*prev = NULL;
+	t_expr	*curr = commands;
+	pid_t		pid;
+	int			status;
+
+	connect_fds(&curr, prev, commands);
+	while (curr != NULL)
+	{
+		//Child process
+		pid = fork();
+		if (pid == 0)
+			execute_commands(&curr);
+		else if (pid < 0)
+			exit(EXIT_FAILURE);
+		// Parent process
+		close_fds(&curr);
+		curr = curr->next;
+	}
+	// Wait for all child processes to finish
+	while (wait(&status) > 0) {}
+}
+
+static void	init_fds(t_expr **commands, t_expr *prev)
+{
+	int		pipefd[2];
+	t_expr	*cur;
+
+	cur = *commands;
+	while (cur->next != NULL)
+	{
+		pipe(pipefd);
+		if (prev != NULL)
+			cur->fd_in = prev->fd_out;
+		else
+			cur->fd_in = STDIN_FILENO;
+		cur->fd_out = pipefd[1];
+		cur->next->fd_in = pipefd[0];
+		prev = cur;
+		cur = cur->next;
+	}
+	if (prev != NULL)
+		cur->fd_in = prev->fd_out;
+	cur->fd_out = 1;
 }
 
 int	pipe_exec(t_msh *msh)
 {
-	t_expr	*cur;
-	char	**expr;
-	int		fd[2];
+	t_expr	*prev = NULL;
+	t_expr	*commands = msh->exp;
 
-	cur = msh->exp->next;
-	pipe(fd);
-	while (cur)
-	{
-		expr = ft_split(cur->data, ' ');
-		pipe_fork(msh, expr, fd);
-		free_split(expr);
-		close(fd[0]);
-		close(fd[1]);
-		cur = cur->next;
-	}
+	init_fds(&commands, prev);
+	execute_multi_pipe(commands);
 	return (0);
 }
