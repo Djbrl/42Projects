3927dfdd86709f2e6088b270587638adeb6dbcf8
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-02-21 17:28:29 +0100

    norminette checkpoint - functions per file next

--- minishell/src/pipes/pipe.c
+++ minishell/src/pipes/pipe.c
@@ -60,6 +60,23 @@ static void	check_paths(t_msh *msh, char **cmd, char *field)
 	free_split(re);
 }
 
+static void	dup_command(t_expr **commands)
+{
+	t_expr	*cur;
+
+	cur = *commands;
+	if (cur->fd_out != 1)
+	{
+		dup2(cur->fd_out, 1);
+		close(cur->fd_out);
+	}
+	if (cur->fd_in != 0)
+	{
+		dup2(cur->fd_in, 0);
+		close(cur->fd_in);
+	}
+}
+
 static void	execute_commands(t_expr **curr_command, t_msh *msh)
 {
 	t_expr	*cur;
@@ -77,22 +94,13 @@ static void	execute_commands(t_expr **curr_command, t_msh *msh)
 		}
 		i++;
 	}
-	if (cur->fd_out != 1)
-	{
-		dup2(cur->fd_out, 1);
-		close(cur->fd_out);
-	}
-	if (cur->fd_in != 0)
-	{
-		dup2(cur->fd_in, 0);
-		close(cur->fd_in);
-	}
+	dup_command(curr_command);
 	cmd = ft_split(cur->data, ' ');
 	check_paths(msh, cmd, cur->data);
 	free_split(cmd);
 }
 
-static void	check_pid_status(t_expr **cmd, t_msh *msh, int tabpid[100], int count)
+static void	check_pid_status(t_expr **cmd, t_msh *msh, int pid[100], int count)
 {
 	t_expr	*curr;
 	int		j;
@@ -112,42 +120,48 @@ static void	check_pid_status(t_expr **cmd, t_msh *msh, int tabpid[100], int coun
 		j = 0;
 		while (j < count)
 		{
-			waitpid(tabpid[j], &g_status, WUNTRACED);
+			waitpid(pid[j], &g_status, WUNTRACED);
 			j++;
 		}
 	}			
+}
+
+static void	child_pipe(t_expr *curr, t_msh *msh)
+{
+	execute_commands(&curr, msh);
+	if (curr->fd_in != 1)
+		close(curr->fd_in);
+	temp_exit(msh);
+	exit(EXIT_SUCCESS);
+}
+
+static void	exit_fork(t_msh *msh)
+{
+	temp_exit(msh);
+	exit(EXIT_FAILURE);
 }
 
 static int	execute_multi_pipe(t_expr *commands, t_msh *msh)
 {
 	t_expr	*curr;
 	int		count;
-	int		tabpid[100];
+	int		pid[100];
 
 	count = 0;
 	curr = commands;
 	connect_fds(&curr, commands);
-	while (curr != NULL)
+	while (curr != NULL && count < 100)
 	{
 		signal(SIGINT, SIG_DFL);
 		signal(SIGQUIT, SIG_DFL);
-		tabpid[count] = fork();
-		if (tabpid[count] == 0)
-		{
-			execute_commands(&curr, msh);
-			if (curr->fd_in != 1)
-				close(curr->fd_in);
-			temp_exit(msh);
-			exit(EXIT_SUCCESS);
-		}
-		else if (tabpid[count] < 0)
-		{
-			temp_exit(msh);
-			exit(EXIT_FAILURE);
-		}
+		pid[count] = fork();
+		if (pid[count] == 0)
+			child_pipe(curr, msh);
+		else if (pid[count] < 0)
+			exit_fork(msh);
 		else
 		{
-			check_pid_status(&curr, msh, tabpid, count);
+			check_pid_status(&curr, msh, pid, count);
 			count++;
 			curr = curr->next;
 		}
