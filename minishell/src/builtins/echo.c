45e80b634987dc8a3004f85b492525acbef3f602
Djibril Sy <dsy@MacBook-Pro-de-Djibril.local>
2023-02-26 19:44:07 +0100

    fixed echo

--- minishell/src/builtins/echo.c
+++ minishell/src/builtins/echo.c
@@ -70,74 +70,73 @@ int	more_than_one_word(char *echo)
 /*
 ** MSH ECHO is ran by msh_echo_runner
 */
-static int	echo_dollar_check(char *arg, t_env_var *env, t_msh *msh)
-{
-	// int		i;
-	// char	*var;
-	char	*tmp;
-	char	*res;
+// static int	echo_dollar_check(char *arg, t_env_var *env, t_msh *msh)
+// {
+// 	// int		i;
+// 	// char	*var;
+// 	char	*tmp;
+// 	char	*res;
 
-	// i = 0;
-	if (ft_strcmp(arg, "$") == 0)
-		ft_putstr("$");
-	else if (ft_strcmp(arg, "$$") == 0)
-		ft_putstr(SHELL_PID_ERROR);
-	else if (ft_strcmp(arg, "$?") == 0)
-	{
-		tmp = ft_strdup("?");
-		res = ft_strdup(get_data_from_env(env, tmp));
-		ft_putstr(res);
-		free(res);
-	}
-	else
-		ft_putstr(arg);
-	(void)msh;
-	// else if (arg[i] == '$' && ft_isalpha(arg[i + 1]) && !msh->single_quote)
-	// 	ftn_msh_echo(&var, &arg, &env, i);
-	return (0);
-}
+// 	// i = 0;
+// 	if (ft_strcmp(arg, "$") == 0)
+// 		ft_putstr("$");
+// 	else if (ft_strcmp(arg, "$$") == 0)
+// 		ft_putstr(SHELL_PID_ERROR);
+// 	else if (ft_strcmp(arg, "$?") == 0)
+// 	{
+// 		tmp = ft_strdup("?");
+// 		res = ft_strdup(get_data_from_env(env, tmp));
+// 		ft_putstr(res);
+// 		free(res);
+// 	}
+// 	else
+// 		ft_putstr(arg);
+// 	(void)msh;
+// 	// else if (arg[i] == '$' && ft_isalpha(arg[i + 1]) && !msh->single_quote)
+// 	// 	ftn_msh_echo(&var, &arg, &env, i);
+// 	return (0);
+// }
 
-static void	more_than_one_word_exec(t_msh *msh, t_env_var *env)
-{
-	int		i;
-	int		j;
-	char	**tmp;
-	char	**args;
+// static void	more_than_one_word_exec(t_msh *msh, t_env_var *env)
+// {
+// 	int		i;
+// 	int		j;
+// 	char	**tmp;
+// 	char	**args;
 
-	j = 1;
-	tmp = ft_split(msh->prompt, '\"');
-	if (tmp[1] == NULL)
-	{
-		args = ft_split(tmp[0], ' ');
-		echo_dollar_check(args[1], env, msh);
-		free_split(args);
-		free_split(tmp);
-		return ;
-	}
-	while (tmp[j])
-	{
-		args = ft_split(tmp[j], ' ');
-		i = 0;
-		while (args[i])
-		{
-			echo_dollar_check(args[i], env, msh);
-			if (args[i + 1])
-				write(1, " ", 1);
-			i++;
-		}
-		free_split(args);
-		i = 0;
-		j++;
-	}
-	free_split(tmp);
-}
+// 	j = 1;
+// 	tmp = ft_split_charset(msh->prompt, "\"\'");
+// 	if (tmp[1] == NULL)
+// 	{
+// 		args = ft_split(tmp[0], ' ');
+// 		echo_dollar_check(args[1], env, msh);
+// 		free_split(args);
+// 		free_split(tmp);
+// 		return ;
+// 	}
+// 	while (tmp[j])
+// 	{
+// 		args = ft_split(tmp[j], ' ');
+// 		i = 0;
+// 		while (args[i])
+// 		{
+// 			echo_dollar_check(args[i], env, msh);
+// 			if (args[i + 1])
+// 				write(1, " ", 1);
+// 			i++;
+// 		}
+// 		free_split(args);
+// 		i = 0;
+// 		j++;
+// 	}
+// 	free_split(tmp);
+// }
 
 int	msh_echo(t_env_var *env, char *arg, t_msh *msh)
 {
-	if (more_than_one_word(arg) == 1)
-		more_than_one_word_exec(msh, env);
-	else
-		echo_dollar_check(arg, env, msh);
+	(void)env;
+	(void)msh;
+	ft_putstr(arg);
 	free(arg);
 	return (0);
 }
